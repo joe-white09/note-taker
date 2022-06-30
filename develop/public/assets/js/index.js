b2aac1df82316d66f591178b358f1d78031676d4
joe-white09 <joe_white09@yahoo.com>
2022-06-30 15:27:53 -0500

    save button hides and the displays as fields are entered

--- develop/public/assets/js/index.js
+++ develop/public/assets/js/index.js
@@ -1,16 +1,9 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
+noteTitle = document.querySelector('.note-title');
+noteText = document.querySelector('.note-textarea');
+saveNoteBtn = document.querySelector('.save-note');
+newNoteBtn = document.querySelector('.new-note');
+noteList = document.querySelectorAll('.list-container .list-group');
 
-if (window.location.pathname === '/notes') {
-  noteTitle = document.querySelector('.note-title');
-  noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('.save-note');
-  newNoteBtn = document.querySelector('.new-note');
-  noteList = document.querySelectorAll('.list-container .list-group');
-}
 
 // Show an element
 const show = (elem) => {
@@ -173,11 +166,11 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes') {
-  saveNoteBtn.addEventListener('click', handleNoteSave);
-  newNoteBtn.addEventListener('click', handleNewNoteView);
-  noteTitle.addEventListener('keyup', handleRenderSaveBtn);
-  noteText.addEventListener('keyup', handleRenderSaveBtn);
-}
+saveNoteBtn.addEventListener('click', handleNoteSave);
+newNoteBtn.addEventListener('click', handleNewNoteView);
+noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+noteText.addEventListener('keyup', handleRenderSaveBtn);
+
+
 
 getAndRenderNotes();
