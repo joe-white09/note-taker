2903392841e83f12442670a67513c284bd3b60e4
joe-white09 <joe_white09@yahoo.com>
2022-07-02 14:47:16 -0500

    new notes added to list

--- routes/apiRoutes/noteRoute.js
+++ routes/apiRoutes/noteRoute.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {createNewNote} = require('../../lib/notes')
 const notes = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
@@ -8,8 +9,23 @@ router.get('/notes', (req, res) => {
     } else {
         res.send(404);
     }
-        
+});
 
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(req.body);
+
+
+});
+
+router.delete(`/api/notes:id`, (req, res) => {
+    let results = notes;
+    if (results) {
+        res.json(results);
+    } else {
+        res.send(404);
+    }
 });
 
 module.exports = router;
