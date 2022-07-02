4af49dd489be094ae750c43cb63976d075afb4a5
joe-white09 <joe_white09@yahoo.com>
2022-07-02 13:01:18 -0500

    displays note database

--- routes/apiRoutes/noteRoute.js
+++ routes/apiRoutes/noteRoute.js
@@ -1,1 +1,15 @@
 const router = require('express').Router();
+const notes = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if (results) {
+        res.json(results);
+    } else {
+        res.send(404);
+    }
+        
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const htmlRoutes = require('./routes/htmlRoutes');
+
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+const htmlRoutes = require('./routes/htmlRoutes/index');
+const apiRoutes = require('./routes/apiRoutes/noteRoute');
 
-// parse incoming string or array data
+
+
+
+
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
 
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`Server now on port ${PORT}`);
