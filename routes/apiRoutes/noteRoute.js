3920e692127c10e6d5ca097fa647e584d2606622
joe-white09 <joe_white09@yahoo.com>
2022-07-02 19:44:06 -0500

    adds notes to database

--- routes/apiRoutes/noteRoute.js
+++ routes/apiRoutes/noteRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const {createNewNote} = require('../../lib/notes')
+const {createNewNote} = require('../../lib/notes');
+const fs = require('fs');
 const notes = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
@@ -12,12 +13,57 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
     req.body.id = notes.length.toString();
     const note = createNewNote(req.body, notes);
     res.json(req.body);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text
+      };
+  
+      // Obtain existing reviews
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new review
+          parsedNotes.push(newNote);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 2),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated Notes!')
+          );
+        }
+      });
 
-
-});
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
 
 router.delete(`/api/notes:id`, (req, res) => {
     let results = notes;
--- server.js
+++ server.js
@@ -1,21 +1,72 @@
 const express = require('express');
-
+const path = require('path');
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const htmlRoutes = require('./routes/htmlRoutes/index');
 const apiRoutes = require('./routes/apiRoutes/noteRoute');
-
-
-
+const notes = require('./db/db.json');
+const {createNewNote} = require('./lib/notes');
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => res.json(notes));
+
+app.post('/api/notes', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text
+      };
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new review
+          parsedNotes.push(newNote);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 2),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.json(response);
+    } else {
+      res.json('Error in posting note');
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`Server now on port ${PORT}`);
