17af429a869d999c0e4ddb1d9e39cc958e55d6af
joe-white09 <joe_white09@yahoo.com>
2022-07-04 17:32:23 -0500

    add heroku

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 const htmlRoutes = require('./routes/htmlRoutes/index');
 const apiRoutes = require('./routes/apiRoutes/noteRoute');
 const notes = require('./db/db.json');
-const {createNewNote} = require('./lib/notes');
+const {createNewNote, validateNote} = require('./lib/notes');
+const noteId = require('./helpers/noteId');
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -18,55 +19,27 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+  res.json(results);
+});
 
 app.post('/api/notes', (req, res) => {
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text
-      };
-      fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-  
-          // Add a new review
-          parsedNotes.push(newNote);
-  
-          // Write updated reviews back to the file
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNotes, null, 2),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated notes!')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.json(response);
-    } else {
-      res.json('Error in posting note');
-    }
-  });
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(notes);
+  }
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server now on port ${PORT}`);
